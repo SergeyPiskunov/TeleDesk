a4e93f1a9b74c07f3460c9d346ce77f2ecab8651
Sergey Piskunov <sergey.piskunoff@gmail.com>
2015-03-14 23:44:13 +0200

    test db connector

--- teledesk.py
+++ teledesk.py
@@ -10,29 +10,24 @@ class DBConnector():
     def create_blank_base(self):
         pass
 
-    def get_data(self, query):
-        with self.db:
-            cursor = self.db.execute(query)
-            column_count = cursor.description.__len__()
-            answer = []
-            for row in cursor:
-                record = {}
-                for column in range(column_count):
-                    record[cursor.description[column][0]] = row[column]
-                answer.append(record)
-            return answer
-
-    def get_data_std(self, query):
+    def get_data(self, query, parameters=None):
 
         with self.db:
             self.db.row_factory = sqlite.Row
-            cur = self.db.cursor()
-            cur.execute(query)
-            return cur.fetchall()
+            cursor = self.db.cursor()
+            if parameters:
+                cursor.execute(query, parameters)
+            else:
+                cursor.execute(query)
+            return cursor.fetchall()
 
 
-dbc = DBConnector("config.db")
-data = dbc.get_data("SELECT * FROM FOLDERS")
-print data[0]["Name"]
-data = dbc.get_data_std("SELECT * FROM FOLDERS")
-print data[0]["Name"]
+if __name__ == "__main__":
+    dbc = DBConnector("config.db")
+    data = dbc.get_data("SELECT * FROM FOLDERS WHERE ID=?", '1')
+    print data
+    data = dbc.get_data("SELECT * FROM FOLDERS WHERE ID=:G", {'G': '1'})
+    print data
+    data = dbc.get_data("SELECT * FROM FOLDERS")
+    print data
+
